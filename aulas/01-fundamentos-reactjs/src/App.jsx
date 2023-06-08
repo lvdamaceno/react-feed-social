5bc89ca77ac496d1dcf9be8bfa4dfd93412a960e
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-06-08 20:13:27 -0300

    update post styles

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,9 @@
-import {Post} from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import {Post} from './components/Post';
+
 import './global.css'
 import styles from './App.module.css'
-import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
---
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,48 @@
+import styles from './Post.module.css';
+
+export function Post(){
+  return(
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img 
+            className={styles.avatar} 
+            src="https://github.com/lvdamaceno.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Vinicius Damaceno</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title='8 de junho as 19:00h' dateTime='2023-06-08 19:00:00'>
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          <a href="">jane.design/doctorcare</a>  
+        </p>
+        <p>
+          <a href="">#novoprojeto </a>
+          <a href="">#nlw </a>
+          <a href="">#rocketseat </a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+
+      <footer>
+        <button type='submit'>Publicar</button>
+      </footer>
+      </form>
+    </article>
+  )
+}
